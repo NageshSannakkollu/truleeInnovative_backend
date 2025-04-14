2d8d8e462697f592015e077a0c16a28ef0c8d4cc
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2025-04-14 10:22:49 +0530

    mongourl updated

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 // const url = process.env.MONGO_URI
 const DbConnection = async() => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}`)
+        await mongoose.connect(`${process.env.MONGO_URI}`,{useNewUrlParser:true,useUnifiedTopology:true})
         console.log("DB Connected Successfully")
     } catch (error) {
         console.log(`DB Error at:${error.message}`)
